317d215d07737c80fa0ca30a15d34b53ac88a5a6
EduardoBenitt <benitt.eduardo@gmail.com>
2023-10-01 22:07:42 -0400

    Criado lista de 10 itens para serem adicionados aletatóriamente no pedido

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Cliente implements Runnable {
     private FilaPedidos filaPedidos;
     private String nome;
+    private List<String> cardapio;
 
     public Cliente(FilaPedidos filaPedidos, String nome) {
         this.filaPedidos = filaPedidos;
         this.nome = nome;
+
+        cardapio = new ArrayList<>();
+        cardapio.add("Hamburguer");
+        cardapio.add("Pizza");
+        cardapio.add("Salada Caesar");
+        cardapio.add("Sopa de Tomate");
+        cardapio.add("Massa Carbonara");
+        cardapio.add("Sushi");
+        cardapio.add("Bife à Milanesa");
+        cardapio.add("Sorvete de Chocolate");
+        cardapio.add("Bolo de Morango");
+        cardapio.add("Torta de Limão");
     }
 
     @Override
@@ -14,24 +29,32 @@ public class Cliente implements Runnable {
         Random random = new Random();
 
         for (int i = 0; i < 3; i++) {
-            String nomePedido = "Pedido " + (i + 1) + " do " + nome;
-            int tempoPreparo = random.nextInt(3901) + 100; // Entre 100 e 4000 ms
+            String nomePedido;
             String categoria;
+
             switch (i) {
                 case 0:
+                    nomePedido = "Entrada do " + nome;
                     categoria = "entrada";
                     break;
                 case 1:
+                    nomePedido = "Prato Principal do " + nome;
                     categoria = "prato principal";
                     break;
                 case 2:
+                    nomePedido = "Sobremesa do " + nome;
                     categoria = "sobremesa";
                     break;
                 default:
+                    nomePedido = "Desconhecido";
                     categoria = "desconhecida";
             }
 
-            Pedido pedido = new Pedido(nomePedido, tempoPreparo, categoria);
+            int tempoPreparo = random.nextInt(3901) + 100; // Entre 100 e 4000 ms
+
+            String itemSelecionado = cardapio.get(random.nextInt(cardapio.size()));
+
+            Pedido pedido = new Pedido(nomePedido, tempoPreparo, itemSelecionado, categoria);
             filaPedidos.adicionarPedido(pedido);
 
             System.out.println(nome + " fez o pedido: " + nomePedido + " (" + categoria + ")");
--- src/Pedido.java
+++ src/Pedido.java
@@ -3,11 +3,13 @@ public class Pedido {
    String nome;
    int tempoPreparo;
    String categoria;
+   String item; // Novo atributo
 
-    public Pedido(String nome, int tempoPreparo, String categoria) {
+    public Pedido(String nome, int tempoPreparo, String categoria, String item) {
         this.nome = nome;
         this.tempoPreparo = tempoPreparo;
         this.categoria = categoria;
+        this.item = item;
     }
 
     public String getNome() {
@@ -33,6 +35,14 @@ public class Pedido {
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
-   
-   
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+    
+    
 }
